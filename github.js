f05df0d5e27d1ad68cd686b5f770c6d46f26ea56
Loki <zpk.loki@gmail.com>
2019-11-14 16:15:20 +0300

    JS Fetch/hiden id

--- github.js
+++ github.js
@@ -19,14 +19,16 @@ class Github {
     this.client_secret = id_s;
 
   }
-
   async getUser(user) {
-    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
 
     const profile = await profileResponse.json();
 
     return {
       profile
-    }
+    };
   }
 }
+
