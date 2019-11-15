97e5939e2ca8dd9760474a5f310836c4f6dabea6
Loki <zpk.loki@gmail.com>
2019-11-15 15:12:12 +0300

    error ui

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ if(userText !== ''){
   .then(data => {
     if(data.profile.message === 'Not Found'){
       // Show alert
-
+      ui.showAlert('User not found', 'alert alert-danger');
 
     } else {
       //Show profile
@@ -31,5 +31,6 @@ if(userText !== ''){
   })
 } else {
   // Clear profile
+  ui.clearProfile();
 }
 })
